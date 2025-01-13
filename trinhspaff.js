b8d055bb3b7ec2d6690c6dde8fda07f5ad4b43ea
bkavw5 <electronicmusic911@gmail.com>
2025-01-14 01:13:04 +0700

    trinhspaff.js

--- trinhspaff.js
+++ trinhspaff.js
@@ -1,23 +1,22 @@
 // Function to set a cookie
 function Set_Cookie(a, b, c, e, f, g) {
-  var d = new Date;
+  var d = new Date();
   d.setTime(d.getTime());
   d = new Date(d.getTime() + c);
   document.cookie = a + "=" + escape(b) + (c ? ";expires=" + d.toGMTString() : "") + (e ? ";path=" + e : "") + (f ? ";domain=" + f : "") + (g ? ";secure" : "");
 }
 
-// Function to get a cookie
+// Function to get a cookie value
 function Get_Cookie(a) {
   var b = document.cookie.indexOf(a + "="),
       c = b + a.length + 1;
-  if (!b && a != document.cookie.substring(0, a.length) || -1 == b)
-    return null;
+  if (!b && a != document.cookie.substring(0, a.length) || -1 == b) return null;
   a = document.cookie.indexOf(";", c);
   -1 == a && (a = document.cookie.length);
   return unescape(document.cookie.substring(c, a));
 }
 
-// Function to randomly select a Shopee link from the list
+// Function to randomly select a Shopee link
 function getRandomShopeeLink() {
   var links = [
     "https://s.shopee.vn/2AzBGedvdZ",
@@ -25,33 +24,34 @@ function getRandomShopeeLink() {
     "https://shopee.vn/affiliate-link-3",
     "https://shopee.vn/affiliate-link-4"
   ];
-  
-  // Select a random index from the links array
   var randomIndex = Math.floor(Math.random() * links.length);
-  
   return links[randomIndex];
 }
 
-// Function to open Shopee link after a delay
-function openShopee() {
+// Function to open Shopee link in a pop-up
+function openShopeePopup() {
   var lastClickTime = Get_Cookie('lastClickTime');
   var currentTime = new Date().getTime();
-  
+
   // If the last click was over 20 minutes ago or no cookie exists
   if (!lastClickTime || (currentTime - lastClickTime >= 1200000)) {
-    // Set a cookie with the current time to track when the last click happened
-    Set_Cookie('lastClickTime', currentTime, 120, '/');
-    
-    // Open a random Shopee link in a new tab
+    // Set a cookie with the current time (20 minutes)
+    Set_Cookie('lastClickTime', currentTime, 1200000, '/');
+
+    // Open the Shopee pop-up
     var link = getRandomShopeeLink();
-    window.open(link, "_blank");
-  }
-  else {
-    console.log("Not enough time has passed since last click.");
+    var popup = window.open(link, "ShopeeAd", "width=800,height=600,scrollbars=yes,resizable=yes");
+    
+    // If pop-up is blocked, show an alert
+    if (!popup) {
+      alert("Pop-up blocked! Please allow pop-ups for this website.");
+    }
+  } else {
+    console.log("Not enough time has passed. Please wait.");
   }
 }
 
-// Attach click event listener to the document
+// Listen for any click event on the page
 document.body.addEventListener("click", function () {
-  openShopee();
+  openShopeePopup();
 });
